5fd655c5e4c217ab7df297dd10022bd85e150a0b
Ritik <ritik.kumar006@gmail.com>
2021-05-11 22:22:14 +0530

    changed to view binding

--- app/src/main/java/com/example/pearlweather/MainActivity.kt
+++ app/src/main/java/com/example/pearlweather/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.pearlweather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pearlweather.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
