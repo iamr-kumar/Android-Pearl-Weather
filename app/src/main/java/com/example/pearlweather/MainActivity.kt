38bfcb6896d00377b54da734b6f4426a8f22c0e2
Ritik <ritik.kumar006@gmail.com>
2021-05-12 04:15:59 +0530

    splash screen and location permissions done

--- app/src/main/java/com/example/pearlweather/MainActivity.kt
+++ app/src/main/java/com/example/pearlweather/MainActivity.kt
@@ -1,15 +1,133 @@
 package com.example.pearlweather
 
+import android.annotation.SuppressLint
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.location.Location
+import android.location.LocationManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
+import android.util.Log
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.pearlweather.databinding.ActivityMainBinding
+import com.google.android.gms.location.*
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        if(!isLocationEnabled()) {
+            Toast.makeText(this,
+                "Your location provider is not turned on. Please turn it on",
+                Toast.LENGTH_SHORT)
+                .show()
+
+            val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivity(intent)
+        } else {
+            Dexter.withActivity(this)
+                .withPermissions(android.Manifest.permission.ACCESS_FINE_LOCATION,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                .withListener(object : MultiplePermissionsListener {
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        permissions: MutableList<PermissionRequest>?,
+                        token: PermissionToken?
+                    ) {
+                        Log.i("DENIED", "Permissions Denied!")
+                        showRationaleDialogForPermissions()
+                    }
+
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                       if(report!!.areAllPermissionsGranted()) {
+                            requestLocationData()
+                       }
+
+                        if(report!!.isAnyPermissionPermanentlyDenied) {
+                            Toast.makeText(this@MainActivity,
+                                "You have denied one or more permissions for this app to work. Please accept the permissions",
+                                 Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                    }
+                }).onSameThread().check()
+        }
+
+        
+
     }
+
+    @SuppressLint("MissingPermission")
+    private fun requestLocationData() {
+        var mLocationRequest = LocationRequest.create().apply {
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            interval = 1000
+            numUpdates = 1
+
+        }
+
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper())
+    }
+
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            val mLastLocation: Location = locationResult.lastLocation
+            val latitude = mLastLocation.latitude
+            val longitude = mLastLocation.longitude
+
+            Log.i("latitude", "$latitude")
+            Log.i("longitude", "$longitude")
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    private fun showRationaleDialogForPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage("It looks like you have turned off one or more permissions required for this app to run. Please turn them on")
+            .setPositiveButton("GO TO SETTINGS") { _,_ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+
+            }
+            .setNegativeButton("CANCEL") {
+                dialog, _ ->
+                    dialog.dismiss()
+            }.show()
+
+    }
+
+
 }
---
+++ app/src/main/java/com/example/pearlweather/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.example.pearlweather
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+
+    private val SPLASH_SCREEN_TIMEOUT: Long = 2000
+    private lateinit var handler: Handler
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_SCREEN_TIMEOUT)
+
+    }
+}
